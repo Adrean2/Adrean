dd8f75798e512005887b755ce4c9917079b047a1
Adrean <adrean337@gmail.com>
2021-07-17 15:24:31 +0200

    Correction bugs dû au merge

--- Temps.py
+++ Temps.py
@@ -3,18 +3,22 @@ import os
 import csv
 import random
 
-# Création fichier CSV
-csvfile = open('D:\Dev\$ COURS OPEN CLASSROOMS\ProjetTemps\CSV.csv','w',encoding='UTF8',newline='')
-writer = csv.writer(csvfile)
-
-# Ajout de données dans le fichier CSV
-listeCSV = []
-while len(listeCSV) <= 20:
-    quantité = random.choice([round(random.uniform(0,9000),1),random.randrange(0,9000,1)])
-    unité = random.choice(["h","s","m"])
-    row = (quantité,unité)
-    listeCSV.append(row)
-    writer.writerow(listeCSV[-1])
+def create_csv():
+    # Création fichier CSV
+    with open('D:\Dev\$ COURS OPEN CLASSROOMS\ProjetTemps\CSV.csv','w',encoding='UTF8',newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # Ajout de données dans le fichier CSV
+        listeCSV = []
+        while len(listeCSV) <= 20:
+            # choisit entre une quantité float ou int, de 0 à 9000
+            quantité = random.choice([round(random.uniform(0,9000),1),random.randrange(0,9000,1)])
+            if quantité >= 30:
+                unité = random.choice(["s","m"])
+            else:
+                unité = random.choice(["h","s","m"])
+            row = (quantité,unité)
+            listeCSV.append(row)
+            writer.writerow(listeCSV[-1])
 # Création conversion de la durée
 def conversion(durée,unité):
     h = 0
@@ -44,9 +48,10 @@ def conversion(durée,unité):
     print("{} heures,{} minutes,{} secondes.".format(h,m,s))
 
 if __name__ == '__main__':
-  with open('D:\Dev\$ COURS OPEN CLASSROOMS\ProjetTemps\CSV.csv','r') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for line in csv_reader:
-      durée = float(line[0])
-      unité = str(line[1])
-      conversion(durée,unité)
+    create_csv()
+    with open('D:\Dev\$ COURS OPEN CLASSROOMS\ProjetTemps\CSV.csv','r',encoding='UTF8') as csv_reader:
+        reader  = csv.reader(csv_reader,delimiter=",")
+        for line in reader:
+            durée = float(line[0])
+            unité = str(line[1])
+            conversion(durée,unité)
